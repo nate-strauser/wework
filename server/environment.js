b38f0ddf1295e95ec7de46ebfd7a3fbaa0df09d3
Nathan Strauser <nathan.strauser@gmail.com>
2014-02-26 11:57:40 -0500

    remove settings - add amp

--- server/environment.js
+++ server/environment.js
@@ -2,4 +2,6 @@ Meteor.startup(function () {
 	AccountsEntry.config({
 		signupCode: null
 	});
+	if(Meteor.settings && Meteor.settings.apm && Meteor.settings.apm.id && Meteor.settings.apm.secret)
+		Apm.connect(Meteor.settings.apm.id, Meteor.settings.apm.secret);
 });
