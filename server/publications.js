ff5d364b5f59d77e7ef9ac597f5e3b77a060adba
Paul Molluzzo <paul@molluzzo.com>
2014-04-10 16:11:54 -0400

    Update publish/subscribe to test created/updated dates; create single job publish/subscribe; create my_jobs publish/subscribe

--- server/publications.js
+++ server/publications.js
@@ -11,8 +11,26 @@ Meteor.publish("userData", function () {
 Meteor.publish("jobs", function(){
 	check(arguments, [Match.Any]);
 	return [
-		Jobs.find({})
+		Jobs.find({$or: [ {"createdAt": { $gte: daysUntilExpiration() }}, {"updatedAt": { $gte: daysUntilExpiration() }}] })
 	];
+});
+
+Meteor.publish("my_jobs", function(){
+    check(arguments, [Match.Any]);
+    if(this.userId){
+        return [
+            Jobs.find({userId:this.userId})
+        ];
+    }
+    this.ready();
+});
+
+// Used for returning single job at permalink
+Meteor.publish("job", function(jobId){
+	check(arguments, [Match.Any]);
+    return [
+        Jobs.find({_id:jobId})
+    ];
 });
 
 Meteor.publish("experts", function(){
