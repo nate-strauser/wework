41d18d9127cb57513e95bd147c35f8fce9394b59
Paul Molluzzo <paul@molluzzo.com>
2014-05-02 21:53:17 -0400

    Improve mobile navigation collapse

--- client/views/includes.js
+++ client/views/includes.js
@@ -3,3 +3,12 @@ Template.header.helpers({
 		return Experts.findOne({userId:Meteor.userId()});
 	}
 });
+
+Template.header.events({
+    'click .navbar-nav a':function() {
+        var targetButton = document.getElementsByClassName('navbar-toggle')[0];
+        if (window.innerWidth < 768){
+            targetButton.click()
+        }
+    }
+});
