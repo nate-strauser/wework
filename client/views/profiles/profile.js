225effcb3f613f14aa774e1c5a78c6adf332288b
Joris Bontje <jorisbontje@gmail.com>
2015-03-06 11:45:47 +0100

    i18n: closes #35, maybe also #45. (WIP)

--- client/views/profiles/profile.js
+++ client/views/profiles/profile.js
@@ -2,7 +2,7 @@ Template.profile.helpers({
   beforeRemove: function() {
     return function(collection, id) {
       var doc = collection.findOne(id);
-      if (confirm('Really delete "' + doc.title + '"?')) {
+      if (confirm(TAPi18n.__("confirm_deletion", doc.title))) {
         this.remove();
         ga("send", "event", "profile", "remove", doc.title);
         Router.go('profiles');
