ef5258e8d304a15d79c41acc2372d118236fec09
Nathan Strauser <nathan.strauser@gmail.com>
2015-12-21 09:52:00 -0500

    fix header dropdown

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,4 +1,17 @@
-Template.header.helpers({
+Template.header.events({
+  'click .navbar-nav a': function(event, template) {
+    var targetButton = document.getElementsByClassName('navbar-toggle')[0];
+    var _this = $(event.currentTarget); 
+
+    if (window.innerWidth < 768) {
+      if( !_this.hasClass('box-user-option') ){
+         targetButton.click()
+      }
+    }
+  }
+});
+
+Template.headerUserMenu.helpers({
   profile: function() {
     return Profiles.findOne({
       userId: Meteor.userId()
@@ -6,7 +19,7 @@ Template.header.helpers({
   }
 });
 
-Template.header.events({
+Template.headerUserMenu.events({
   'click #signOut': function(event, template) {
     Meteor.logout();
     Router.go("/");
@@ -25,4 +38,8 @@ Template.header.events({
     event.preventDefault();
     Modal.show('userProfile');
   }
+});
+
+Template.headerUserMenu.onRendered(function(){
+  this.$('.dropdown-toggle').dropdown()
 });
