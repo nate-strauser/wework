1a5fdfcbdb22b64a2a9e1dedcdd102b1b9b21163
sasikanth513 <dronavalli.sasi@gmail.com>
2016-11-23 22:09:43 +0530

    job type filter in jobs page

--- client/views/jobs/jobs.js
+++ client/views/jobs/jobs.js
@@ -1,19 +1,45 @@
 Template.jobs.onCreated(function() {
+  this.query = new ReactiveVar({});
   this.infiniteScroll({
     perPage: 30,
     subManager: subs,
     collection: Jobs,
-    publication: 'jobs'
+    publication: 'jobs',
+    query: this.query
   });
 });
 
 Template.jobs.helpers({
   "jobs": function() {
-    return Jobs.find({}, {
+
+    var query = Template.instance().query.get();
+    var options = {
       sort: {
         featuredThrough: -1,
         createdAt: -1
       }
-    });
+    };
+
+    let jobs = Jobs.find(query, options);
+
+    if(jobs.count() > 0){
+      return jobs;
+    }
+
+    return false;
+  },
+  "JOB_TYPES": function() {
+    return JOB_TYPES;
   }
 })
+
+Template.jobs.events({
+  'change #jobType': function (event, template) {
+    let type = event.currentTarget.value;
+    if(type === "All") {
+      template.query.set({})
+    }else{
+      template.query.set({jobtype: type})
+    }
+  }
+});
